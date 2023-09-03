328d197d7faa81d4ab1792262967e8bbfe0f35fc
korobprog <korobprog@gmail.com>
2023-09-03 22:06:46 +1000

    firebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://new-blog-prabhupada.web.app',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(),
+	robotsTxt(
+		{
+			allow: '/',
+			userAgent: '*'
+		}
+	)]
 });
