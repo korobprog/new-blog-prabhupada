264dda793619d03ff50f6baaf44b992ebfae5695
korobprog <korobprog@gmail.com>
2023-09-03 18:32:26 +1000

    финиш сделал блог на сегодня

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import.meta.env.BASE_URL
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://korobprog.github.io/new-blog-prabhupada',
+	site: 'https://new-blog-prabhupada.web.app',
 	integrations: [mdx(), sitemap()],
-	base: '/new-blog-prabhupada',
 });
---
+++ src/firebase/client.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxUD4nwoRjrY6c8S2k5jQI21djAX8B7Ro",
+  authDomain: "new-blog-prabhupada.firebaseapp.com",
+  projectId: "new-blog-prabhupada",
+  storageBucket: "new-blog-prabhupada.appspot.com",
+  messagingSenderId: "841556728066",
+  appId: "1:841556728066:web:160a0002e39234734bca1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
