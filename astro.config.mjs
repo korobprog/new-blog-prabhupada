5503eb3de46f7e5563ae5cdad80a41b99d91a080
korobprog <korobprog@gmail.com>
2023-09-03 10:17:05 +1000

    ccd deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import.meta.env.BASE_URL
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://korobprog.github.io',
 	integrations: [mdx(), sitemap()],
+	base: '/new-blog-prabhupada',
 });
